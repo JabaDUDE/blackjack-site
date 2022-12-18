fb1b6321b823f8a83726655420a479e2471eda9d
JabaDUDE <dakotawkeast@gmail.com>
2022-12-18 00:23:01 -0500

    api link up

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const url = 'https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6'
 
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data)
-  })
-  .catch((err) => console.log(`error: ${err.message}`))
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => console.log(`error: ${err.message}`))
+}
